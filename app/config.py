573dfad5c0e9b49511f1abd4c97ada304789109e
SvVov <96830499+SvVov@users.noreply.github.com>
2025-08-09 15:32:29 +0300

    removed var from config.py

--- app/config.py
+++ app/config.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     APP_NAME: str = "Telegram Bot Webhook Server"
     WEBHOOK_SECRET_URL_PART: str
     TELEGRAM_BOT_TOKEN: str
-    GOOGLE_APPLICATION_CREDENTIALS: str 
 
     class Config:
         env_file = ".env"  # .env файл буде використовуватись локально, у проді змінні з ENV
--- app/translator.py
+++ app/translator.py
@@ -6,16 +6,16 @@ import logging
 
 logger = logging.getLogger("webhook")
 
-SERVICE_ACCOUNT_FILE = settings.GOOGLE_APPLICATION_CREDENTIALS
-SCOPES = ["https://www.googleapis.com/auth/cloud-translation"]
+# SERVICE_ACCOUNT_FILE = settings.GOOGLE_APPLICATION_CREDENTIALS
+# SCOPES = ["https://www.googleapis.com/auth/cloud-translation"]
 
-if SERVICE_ACCOUNT_FILE and os.path.exists(SERVICE_ACCOUNT_FILE):
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES
-    )
-    client = translate.Client(credentials=credentials)
-else:
-    client = translate.Client()
+# if SERVICE_ACCOUNT_FILE and os.path.exists(SERVICE_ACCOUNT_FILE):
+#     credentials = service_account.Credentials.from_service_account_file(
+#         SERVICE_ACCOUNT_FILE, scopes=SCOPES
+#     )
+#     client = translate.Client(credentials=credentials)
+# else:
+client = translate.Client()
 
 
 def translate_text(text: str, target_lang: str = "uk") -> str:
